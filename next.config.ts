fbf3902cbd913d7ec8706a98d3739c7b977f4fbf
Muhammad Rafli Naufal <muhamadraflinaufal@gmail.com>
2026-02-24 09:24:50 +0700

    update: required files

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 };
 
 export default nextConfig;
